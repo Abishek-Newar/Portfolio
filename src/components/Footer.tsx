2a12e192206615c3419928ec80ae476f1eeeffbe
abishejk1872 <abisheknewar15@gmail.com>
2024-02-01 04:59:57 +0530

    changes in link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,10 @@ export default function Footers(){
     return <div className={`flex  items-center justify-around w-full select-none ${theme == "dark"? "bg-bgds": "bg-white"} `}>
     <div className={`flex flex-col lg:flex-row w-full justify-between ${theme == "dark"? "text-white border-forHover":"text-bgds border-red-300"} bg-transparent backdrop-blur-sm  px-10 py-6  h-auto lg:mx-56 my-7 border-[0.1px] rounded-lg `}>
         <nav className={` flex items-center font-chill text-end gap-10 transition-all ease-linear duration-300  `}>
-
-           <FaLinkedin className="w-9 h-9" />
-            <FaSquareGithub className="w-9 h-9" />
-            <FaXTwitter className="w-9 h-9" />
+            <a href="https://linkedin.com/in/abishek-newar-a67141225"><FaLinkedin className="w-9 h-9" /></a>
+            <a href="https://github.com/Abishek-Newar"><FaSquareGithub className="w-9 h-9" /></a>
+            <a href="https://x.com/abiiNewar?t=Hgrz-uUBA8tNP_UC19CYZA&s=09"><FaXTwitter className="w-9 h-9" /></a>
+            
         </nav>
         <div >
         <div className="flex items-center mb-2 gap-2">
