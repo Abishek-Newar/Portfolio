16abb1b5d13041e5b53e0f80c44e6c2ccc87faa7
Abishek Newar <abisheknewar15@gmail.com>
2026-01-07 00:52:04 +0530

    changes in tech stack, age , experience

--- src/components/aboutDivs.tsx
+++ src/components/aboutDivs.tsx
@@ -1,15 +1,15 @@
 
 
 import Contributions from "./Contributions";
-export default function AboutDivs(){
+export default function AboutDivs() {
     const theme = localStorage.getItem("modes")
     const cards = [{
         title: "About Me",
-        description: "Hello! I am Abishek, a passionate 22-year-old Full Stack developer from the lively city of kokrajhar,Assam. I enjoy in crafting digital experiences"
-    },{
+        description: "Hello! I am Abishek, a passionate 23-year-old Full Stack developer from the lively city of kokrajhar,Assam. I enjoy in crafting digital experiences"
+    }, {
         title: "Professional Background",
         description: "I’m a Full Stack Developer specializing in Next.js. My expertise spans both front-end and back-end development, working with technologies like React, Node.js, Express, MongoDB, PostgreSQL, and Prisma. I focus on building scalable, efficient, and user-friendly web applications."
-    },{
+    }, {
         title: "Current Focus",
         description: "My current focus is on full-stack development using React, Node.js, and Next.js. I am continuously working to deepen my expertise in building scalable and efficient web applications. Additionally, I am eager to learn DevOps practices to enhance deployment, automation, and system maintenance."
     }]
@@ -18,13 +18,13 @@ export default function AboutDivs(){
         degree: "B.E",
         year: "2020 - 2024",
         college: "Chadigarh University"
-    },{
+    }, {
         degree: "Senior Secondary",
         year: "2018-2020",
         college: "Caesar School"
-    },{
+    }, {
         degree: "Matriculation",
-        year : "2017-2018",
+        year: "2017-2018",
         college: "Caesar School"
     }]
 
@@ -32,44 +32,48 @@ export default function AboutDivs(){
         role: "Full Stack Developer",
         company: "Infotechmon Pvt. Ltd.",
         year: "April 2024 - Feb 2025"
-    },{
+    }, {
         role: "Web Developer",
         company: "Cool Plugins",
         year: "Mar 2025 - Oct 2025"
+    }, {
+        role: "Junior Developer",
+        company: "Webethics Solutions",
+        year: "Nov 2025 - Present"
     }]
-    return <div className={` font-chill flex flex-col pt-64 pb-10   items-center justify-center ${theme != "light"? "bg-bgds":"bg-white"} gap-10`}>
-            <div className={`flex flex-wrap gap-8  items-center justify-center  text-texts ` }>
-                {cards.map((card)=>(
-                    <div className={` w-[350px] h-[400px] border-2 rounded-lg p-10 ${theme != "light"? "border-brdr": "border-red-300"} transition-all ease-in-out duration-500 hover:translate-y-[-7px]`}>
-                        <h1 className="text-2xl font-semibold mb-6">{card.title}</h1>
-                         <p>{card.description}</p>
-                    </div>
-                ))}
-            </div>
-            <Contributions theme={theme as string} />
-            <div className={`border-2 w-[74%] rounded-lg ${theme != "light"? "border-brdr": "border-red-300" } p-8`}  >
-                <h1 className={` text-3xl mb-8  ${theme != "light"? "text-white":"text-bgds"} font-bold`}>Experience</h1>
-                {experience.map((edu)=>(
-                    <div className="text-spcl mb-5">
-                        <div className="flex justify-between">
-                            <h1 className="text-xl text-texts text-semibold " >{edu.role}</h1>
-                            <h2>{edu.year}</h2>
-                        </div>
-                        <h3>{edu.company}</h3>
-                    </div>
-                ))}
-            </div>
-            <div className={`border-2 w-[74%] rounded-lg ${theme != "light"? "border-brdr": "border-red-300" } p-8`}  >
-                <h1 className={` text-3xl mb-8  ${theme != "light"? "text-white":"text-bgds"} font-bold`}>Education</h1>
-                {education.map((edu)=>(
-                    <div className="text-spcl mb-5">
-                        <div className="flex justify-between">
-                            <h1 className="text-xl text-texts text-semibold " >{edu.degree}</h1>
-                            <h2>{edu.year}</h2>
-                        </div>
-                        <h3>{edu.college}</h3>
-                    </div>
-                ))}
-            </div>
+    return <div className={` font-chill flex flex-col pt-64 pb-10   items-center justify-center ${theme != "light" ? "bg-bgds" : "bg-white"} gap-10`}>
+        <div className={`flex flex-wrap gap-8  items-center justify-center  text-texts `}>
+            {cards.map((card) => (
+                <div className={` w-[350px] h-[400px] border-2 rounded-lg p-10 ${theme != "light" ? "border-brdr" : "border-red-300"} transition-all ease-in-out duration-500 hover:translate-y-[-7px]`}>
+                    <h1 className="text-2xl font-semibold mb-6">{card.title}</h1>
+                    <p>{card.description}</p>
+                </div>
+            ))}
         </div>
+        <Contributions theme={theme as string} />
+        <div className={`border-2 w-[74%] rounded-lg ${theme != "light" ? "border-brdr" : "border-red-300"} p-8`}  >
+            <h1 className={` text-3xl mb-8  ${theme != "light" ? "text-white" : "text-bgds"} font-bold`}>Experience</h1>
+            {experience.map((edu) => (
+                <div className="text-spcl mb-5">
+                    <div className="flex justify-between">
+                        <h1 className="text-xl text-texts text-semibold " >{edu.role}</h1>
+                        <h2>{edu.year}</h2>
+                    </div>
+                    <h3>{edu.company}</h3>
+                </div>
+            ))}
+        </div>
+        <div className={`border-2 w-[74%] rounded-lg ${theme != "light" ? "border-brdr" : "border-red-300"} p-8`}  >
+            <h1 className={` text-3xl mb-8  ${theme != "light" ? "text-white" : "text-bgds"} font-bold`}>Education</h1>
+            {education.map((edu) => (
+                <div className="text-spcl mb-5">
+                    <div className="flex justify-between">
+                        <h1 className="text-xl text-texts text-semibold " >{edu.degree}</h1>
+                        <h2>{edu.year}</h2>
+                    </div>
+                    <h3>{edu.college}</h3>
+                </div>
+            ))}
+        </div>
+    </div>
 }
--- src/components/techStack.tsx
+++ src/components/techStack.tsx
@@ -1,72 +1,72 @@
 
 
-export default function TechStack(){
+export default function TechStack() {
     const techs = [{
         name: "React",
         link: "https://skillicons.dev/icons?i=react"
-    },{
+    }, {
         name: "Node",
         link: "https://skillicons.dev/icons?i=nodejs"
-    },{
+    }, {
         name: "Tailwind",
         link: "https://skillicons.dev/icons?i=tailwind"
-    },{
+    }, {
         name: "javaScript",
         link: "https://skillicons.dev/icons?i=javascript"
-    },{
+    }, {
         name: "mongoDB",
         link: "https://skillicons.dev/icons?i=mongo"
-    },{
+    }, {
         name: "github",
         link: "https://skillicons.dev/icons?i=github"
-    },{
+    }, {
         name: "TypeScript",
         link: "https://skillicons.dev/icons?i=typescript"
-    },{
+    }, {
         name: "PostgreSQL",
         link: "https://skillicons.dev/icons?i=postgres"
-    },{
+    }, {
         name: "Prisma",
         link: "https://skillicons.dev/icons?i=prisma"
-    },{
+    }, {
         name: "Docker",
         link: "https://skillicons.dev/icons?i=docker"
     }]
     const skills = [{
         name: "Frontend",
-        skill: ["HTML","CSS","React","Tailwind","NextJS"],
-    },{
+        skill: ["HTML", "CSS", "React", "Tailwind", "NextJS"],
+    }, {
         name: "Backend",
-        skill: ["Node","Express","Hono","PostgreSQL","Prisma","MongoDB"]
-    },{
+        skill: ["Node.js", "Express", "Nest.js", "Hono", "FastAPI", "PostgreSQL", "PineCone", "Prisma", "MongoDB"]
+    }, {
         name: "Others",
-       skill: ["TypeScript","JavaScript","Python","PHP","Docker","Git", "Github","AWS","Wordpress", "Wordpress Plugin Development"]
+        skill: ["TypeScript", "JavaScript", "Python", "PHP", "Docker", "Git", "Github", "AWS", "Wordpress", "Wordpress Plugin Development"]
     }]
-    
+
     const theme = localStorage.getItem("modes");
-    return <div className={`${theme != "light"? "bg-bgds text-white":"bg-white text-bgds"} mx-auto select-none `}>
+    return <div className={`${theme != "light" ? "bg-bgds text-white" : "bg-white text-bgds"} mx-auto select-none `}>
         <h1 className="text-center text-4xl font-semibold mb-10  ">Tech Stack</h1>
-        <div className={ `flex flex-wrap gap-4 justify-center mb-32`}>
-            {techs.map((tech)=>(
-                <div className={`group transition-all ease-in-out duration-500 w-24 h-24 border-2  ${theme!= "light"? "border-brdr hover:bg-forHover":"border-red-300 hover:bg-red-300"} flex items-center justify-center rounded-lg `}>
+        <div className={`flex flex-wrap gap-4 justify-center mb-32`}>
+            {techs.map((tech) => (
+                <div className={`group transition-all ease-in-out duration-500 w-24 h-24 border-2  ${theme != "light" ? "border-brdr hover:bg-forHover" : "border-red-300 hover:bg-red-300"} flex items-center justify-center rounded-lg `}>
                     <img className="transition-all ease-in-out duration-500  group-hover:scale-110" src={tech.link} alt={tech.name} />
                 </div>
             ))}
         </div>
         <div className="flex flex-wrap gap-8 p-10 font-chill  justify-center ">
-            
+
             {
-                skills.map((item,index)=>(
-                    <div key={index} className={`border-2 w-[450px] h-[350px]   p-16 rounded-lg transition-all ease-in-out duration-500 ${theme != "light"? "bg-bgds border-brdr hover:bg-forHover": "bg-white border border-red-300 hover:bg-red-300"} `}>
-                    <h1 className="text-center text-lg font-semibold mb-10">{item.name}</h1>
-                    <div className="flex flex-wrap justify-center gap-4">
-                    {item.skill.map((front,index)=>(
-                        <div key={index} className={`border rounded-md h-8 items-center ${item.name =="Others" && front =="Wordpress Plugin Development"? 'w-auto p-2': 'w-20' } flex py-1 justify-center transition-all ease-in-out duration-500  ${theme != "light"? "bg-forHover border-brdr hover:bg-bgds": "bg-red-300 border-white hover:bg-white"} `}>
-                            <h3 className="text-xs font-light">{front}</h3>
+                skills.map((item, index) => (
+                    <div key={index} className={`border-2 w-[450px] h-[350px]   p-16 rounded-lg transition-all ease-in-out duration-500 ${theme != "light" ? "bg-bgds border-brdr hover:bg-forHover" : "bg-white border border-red-300 hover:bg-red-300"} `}>
+                        <h1 className="text-center text-lg font-semibold mb-10">{item.name}</h1>
+                        <div className="flex flex-wrap justify-center gap-4">
+                            {item.skill.map((front, index) => (
+                                <div key={index} className={`border rounded-md h-8 items-center ${item.name == "Others" && front == "Wordpress Plugin Development" ? 'w-auto p-2' : 'w-20'} flex py-1 justify-center transition-all ease-in-out duration-500  ${theme != "light" ? "bg-forHover border-brdr hover:bg-bgds" : "bg-red-300 border-white hover:bg-white"} `}>
+                                    <h3 className="text-xs font-light">{front}</h3>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-            </div>
+                    </div>
                 ))
             }
         </div>
